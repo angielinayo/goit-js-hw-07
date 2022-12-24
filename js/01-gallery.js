94440e45053c3a897726f8775635c22d68a3420e
Anhelina Shabrina <sh.angelina97@gmail.com>
2022-12-24 21:26:25 +0200

    task 1 done

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,46 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+const gallery = document.querySelector('.gallery');
+const galleryMarkup = createGalleryMarkup(galleryItems);
+let instance;
+
+gallery.insertAdjacentHTML('beforeend', galleryMarkup);
+gallery.addEventListener('click', onGalleryClick);
+gallery.addEventListener('keydown', onEscapeClose);
+
+function createGalleryMarkup(galleryItems) {
+  return galleryItems
+    .map(({ preview, original, description }) => {
+      return `<div class="gallery__item">
+    <a class="gallery__link" href='${original}'>
+      <img
+        class="gallery__image"
+        src='${preview}'
+        data-source="${original}"
+        alt='${description}'
+      />
+    </a>
+  </div>`;
+    })
+    .join('');
+}
+
+function onGalleryClick(event) {
+  event.preventDefault();
+  if (event.target.nodeName !== 'IMG') {
+    return;
+  }
+
+  const largeImage = event.target.dataset.source;
+  instance = basicLightbox.create(`
+	<img src="${largeImage}" width="800" height="600">
+`);
+  instance.show();
+}
+
+function onEscapeClose(event) {
+  if (event.code === 'Escape' && instance.visible()) {
+    instance.close();
+  }
+}
