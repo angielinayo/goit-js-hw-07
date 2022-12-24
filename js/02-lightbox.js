d79a4da561c7a85b4cde79a02edd1e5e5e9b1265
Anhelina Shabrina <sh.angelina97@gmail.com>
2022-12-24 21:51:58 +0200

    task 2 done

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,20 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
+const gallery = document.querySelector('.gallery');
+const galleryMarkup = createGalleryMarkup(galleryItems);
 
-console.log(galleryItems);
+gallery.insertAdjacentHTML('beforeend', galleryMarkup);
+
+function createGalleryMarkup(galleryItems) {
+  return galleryItems
+    .map(({ preview, original, description }) => {
+      return `<a class="gallery__item" href='${original}'>
+			<img class="gallery__image" src='${preview}' alt='${description}' />
+		</a>`;
+    })
+    .join('');
+}
+new SimpleLightbox('.gallery a', {
+  captionsData: 'alt',
+  captionDelay: 250,
+});
